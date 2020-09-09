d0c2d08fb306840e4319c16cb35e865b3b99a36f
Paweł Antosiak <pantosiak@gwo.pl>
2020-09-09 16:03:18 +0200

    Add FormatterFactoryTest

---
+++ tests/Http/Formatter/FormatterFactoryTest.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Http\Formatter;
+
+use Gorynych\Http\Exception\NotAcceptableHttpException;
+use Gorynych\Http\Formatter\FormatterFactory;
+use Gorynych\Http\Formatter\JsonFormatter;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Config\FileLocator;
+
+class FormatterFactoryTest extends TestCase
+{
+    public function testCreatesFormatter(): void
+    {
+        $formatter = $this->setUpFormatterFactory()->create('application/json');
+
+        $this->assertInstanceOf(JsonFormatter::class, $formatter);
+    }
+
+    public function testThrowsExceptionOnNotAcceptableFormat(): void
+    {
+        $this->expectException(NotAcceptableHttpException::class);
+
+        $this->setUpFormatterFactory()->create('application/xml');
+    }
+
+    private function setUpFormatterFactory(): FormatterFactory
+    {
+        return new FormatterFactory(new FileLocator(__DIR__ . '/Resource'));
+    }
+}
