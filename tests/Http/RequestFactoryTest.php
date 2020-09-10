95786b793b85272e7650aacf953a19f1dee966f4
Paweł Antosiak <pantosiak@gwo.pl>
2020-09-10 12:00:28 +0200

    Add RequestFactoryTest

---
+++ tests/Http/RequestFactoryTest.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Http;
+
+use Gorynych\Http\RequestFactory;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpFoundation\Request;
+
+class RequestFactoryTest extends TestCase
+{
+    public function testCreatesRequest(): void
+    {
+        $_ENV['BASE_URI'] = 'http://localhost';
+
+        $request = (new RequestFactory())->create(Request::METHOD_GET, '/resources', [
+            RequestFactory::REQUEST_JSON => ['foo' => 'bar']
+        ]);
+
+        $this->assertSame(json_encode(['foo' => 'bar']), $request->getContent());
+        $this->assertSame(Request::METHOD_GET, $request->getMethod());
+        $this->assertSame('http://localhost/resources', $request->getUri());
+        $this->assertSame('application/json', $request->headers->get('Accept'));
+        $this->assertSame('application/json', $request->headers->get('Content-Type'));
+    }
+}
